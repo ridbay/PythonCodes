fbe2f138e421d39d8b0271c52cc3340289c1a8a2
Balogun Ridwan Babatunde <balogunridwan@gmail.com>
2018-10-17 13:08:20 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
-from datetime import datetime
-date1 = datetime(year = 1989, month = 4, day = 21)
+from datetime import timedelta
+name = input("What\'s your name: ")
+print("Enter your DOB to know how long you have lived")
+
+#accept input from user
+
+year_input = int(input("Enter your year of birth e.g \'1990': "))
+month_input = int(input("Enter your month of birth e.g \'4' for april: "))
+day_input = int(input("Enter your day of birth e.g \'21' "))
+date1 = datetime(year = year_input, month = month_input, day = day_input)
 date2 = datetime.today()
 total = date2 - date1
-print(total)
+print("\n\n")
+print("Hi", name)
+print("You have lived ",total)
 total2 = total.total_seconds()
-print(total2)
+print("\n\n")
+print("and \n\n You have lived ", total2,"seconds")
