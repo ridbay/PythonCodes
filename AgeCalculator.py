11ce3656d744eb980d5dd2a2e1b12e6120280f31
Balogun Ridwan Babatunde <balogunridwan@gmail.com>
2018-10-17 14:38:27 +0100

    Update AgeCalculator.py
    Enter your DOB to know how long you have lived and how many days

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,18 +1,34 @@
 from datetime import timedelta
 name = input("What\'s your name: ")
-print("Enter your DOB to know how long you have lived")
+print("Enter your DOB to know how long you have lived and how many days")
 
 #accept input from user
 
 year_input = int(input("Enter your year of birth e.g \'1990': "))
 month_input = int(input("Enter your month of birth e.g \'4' for april: "))
-day_input = int(input("Enter your day of birth e.g \'21' "))
+day_input = int(input("Enter your day of birth e.g \'21':"))
+
 date1 = datetime(year = year_input, month = month_input, day = day_input)
-date2 = datetime.today()
-total = date2 - date1
+#Today's date and time
+today_date = datetime.today()
+
+#subtract today's date from the user' DOB
+total = today_date - date1
+
+#Next birthday date is(format to remove all date and time except the year(current year))
+date3 = today_date.strftime("%Y")
+
+#current year in str plus 1
+nextyeardate = int(date3) + 1
+#next DOB is current year plus user's month and day input
+datenextyear = datetime(year = nextyeardate, month = month_input, day = day_input)
+days_left = datenextyear - today_date
+day_left = days_left.days
+
 print("\n")
 print("Hi", name)
 print("You have lived ",total.days,"days")
 total2 = total.total_seconds()
 print("and")
 print("You have lived ", total2,"seconds")
+print("You have",day_left,"to your next birthday")
